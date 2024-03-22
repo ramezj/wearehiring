16ecd9ca278aaa989b8d430975a46dd33c6421d2
daddylunix <ramezjoseph8@gmail.com>
2024-03-22 15:46:14 +0200

    added border-b

--- components/Navigation/site-header.tsx
+++ components/Navigation/site-header.tsx
@@ -9,7 +9,7 @@ import { signIn } from "next-auth/react"
 
 export function SiteHeader(props:any) {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/70 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center p-3">
         <MainNav />
         <MobileNav />
