94d06c9d1c74bc665d9375a26b6a19b79678fa94
daddylunix <ramezjoseph8@gmail.com>
2024-03-22 23:29:50 +0200

    fixed width for desktop :D

--- components/Navigation/site-header.tsx
+++ components/Navigation/site-header.tsx
@@ -10,7 +10,7 @@ import { signIn } from "next-auth/react"
 export function SiteHeader(props:any) {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/60 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center p-3">
+      <div className="container flex h-14 items-center p-3 w-full max-w-full">
         <MainNav />
         <MobileNav />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
