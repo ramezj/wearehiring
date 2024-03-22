ab1b3d16400a8e2b573d174c4688f5db4209e2c7
daddylunix <ramezjoseph8@gmail.com>
2024-03-22 23:33:57 +0200

    fixed min-h in /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -47,7 +47,7 @@ export default function Dashboard() {
   }})
   return (
     <Layout session={session}>
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex w-full flex-col">
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
           <Card>
