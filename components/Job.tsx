158a9c0b9ac253e04101dd933cee78a8c379f9be
daddylunix <ramezjoseph8@gmail.com>
2024-03-23 23:03:36 +0200

    changed border color

--- components/Job.tsx
+++ components/Job.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 
 export function Job(props:any) {
     return (
-      <div className="w-full flex bg-white border border-black/10 hover:border-black/30 rounded-lg items-center duration-300">
+      <div className="w-full flex bg-white border border-black/20 rounded-lg items-center duration-300">
       <div className="m-8 flex flex-col items-start text-left">
       <p className='sm:text-lg text-md font-bold text-left text-black dark:text-white'>
        {props.title}     
