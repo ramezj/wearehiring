476a2daef14dc8870c388a8222686063f24fd1fa
Ramez Joseph <ramezjoseph8@gmail.com>
2024-03-26 02:35:27 +0200

    landing page Job component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useSession } from "next-auth/react";
 import { Layout } from "@/components/Layouts/MainLayout";
 import { Badge } from "@/components/ui/badge";
 import { HologramCard } from "@/components/HologramCard";
+import { Zap } from "lucide-react";
+import { Job } from "@/components/Job";
 
 
 export default function Home() {
@@ -13,10 +15,17 @@ export default function Home() {
     <Layout session={session}>
     <div className="text-center w-full">
       <br /><br />
-      <Badge className="px-5 py-1 2xl:px-12 2xl:py-2 text-sm">wearehiring is currently in pre-alpha.</Badge>
+      <Badge className="px-5 py-1 2xl:px-12 2xl:py-2 text-sm gap-2"><Zap className='w-4 h-4' />wearehiring is currently in pre-alpha</Badge>
       <br /><br />
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">Simplifying the hiring process.</h1>
         <br />
+        <div className="w-full flex items-center justify-center">
+          <div className="md:w-3/5 w-full">
+            <Job title={"Back-End Engineer"} buttonText={"Apply"} type={"Full-Time"} location={"Los Angeles"}/>
+            <br />
+            <Job title={"Product Manager"} buttonText={"Apply"} type={"Full-Time"} location={"New York"}/>
+          </div>
+        </div>
     </div>
     </Layout>
     </>
