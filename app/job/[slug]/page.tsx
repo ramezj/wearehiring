81af3a3f95a31a63c707374fc095e844c173d71c
Ramez Joseph <ramezjoseph8@gmail.com>
2024-03-26 09:27:17 +0200

    idk

--- app/job/[slug]/page.tsx
+++ app/job/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { Layout } from "@/components/Layouts/MainLayout";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Briefcase, Navigation, DollarSign, DollarSignIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Page({ params }: { params: { slug: string }}) {
     const [ loading, setLoading ] = useState<boolean>(true);
@@ -42,6 +44,19 @@ export default function Page({ params }: { params: { slug: string }}) {
             <Badge className="rounded-sm"><DollarSign className="w-3 h-3 mr-1" />{job?.salary as number}</Badge>
             </div>
             <br />
+        <div className="w-4/5">
+            <form>
+            <Label>Full Name (required)</Label>
+            <Input className="md:w-3/5 w-full"/>
+            <Label>Email Address (required)</Label>
+            <Input className="md:w-3/5 w-full"/>
+            <br />
+            <Button variant={"outline"} >Upload Resume </Button>
+            <br />
+            <Label>Phone Number (required)</Label>
+            <Input className="md:w-3/5 w-full"/>
+            </form>
+        </div>
             <Button> Apply to job </Button>
             {/* {JSON.stringify(job)} */}
             </div>
